42cf8a2163023b7725486d5ea265342084805c85
alwaysamer <amer3107@gmail.com>
2024-07-15 00:43:58 +0200

    fixed a bug in interactive selection and refactored a bit

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,7 +1,7 @@
 use crate::core::config::Config;
 use std::{
     fs::{self, OpenOptions},
-    io::{self},
+    io::{self, ErrorKind},
     path::Path,
     process::Command,
 };
@@ -35,41 +35,47 @@ pub fn path_exists<P: AsRef<Path>>(path: P) -> bool {
     path.exists()
 }
 
-pub fn find_projects(dir: String) -> Option<Vec<(String, String)>> {
+pub fn find_projects(dir: String) -> Result<Vec<(String, String)>, io::Error> {
     let mut projects = Vec::new();
     let dir = Path::new(&dir);
 
-    if let Ok(entries) = fs::read_dir(dir) {
-        for entry in entries.filter_map(Result::ok) {
-            let path = entry.path();
-            if path.is_dir() {
-                let start_md_path = path.join("start.md");
-                if start_md_path.exists() {
-                    if let Some(parent) = path.file_name().and_then(|n| n.to_str()) {
-                        projects.push((
-                            String::from(start_md_path.to_str().unwrap()),
-                            parent.to_string(),
-                        ))
-                    }
+    let entries = fs::read_dir(dir).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    for entry in entries.filter_map(Result::ok) {
+        let path = entry.path();
+        if path.is_dir() {
+            let start_md_path = path.join("start.md");
+            if start_md_path.exists() {
+                if let Some(parent) = path.file_name().and_then(|n| n.to_str()) {
+                    projects.push((
+                        String::from(start_md_path.to_str().unwrap()),
+                        parent.to_string(),
+                    ))
                 }
             }
         }
-        Some(projects)
-    } else {
-        None
     }
+    Ok(projects)
 }
 
 pub fn select_project(config: Config) -> Result<String, io::Error> {
     let projects_dir = config.core.note_dir + "/projects";
-    let projects = find_projects(projects_dir).unwrap();
+    let projects = match find_projects(projects_dir) {
+        Ok(p) => p,
+        Err(_) => {
+            return Err(io::Error::new(
+                ErrorKind::NotFound,
+                "Couldn't find or access either the projects-dir or even the whole note-dir.",
+            ))
+        }
+    };
     if projects.is_empty() {
         return Err(io::Error::new(
             io::ErrorKind::NotFound,
             "No Projects found.",
         ));
     }
-    cliclack::intro(console::style(" NoJo ").on_cyan().black()).unwrap();
+    cliclack::intro(console::style(" Grom ").on_cyan().black()).unwrap();
 
     let mut items: Vec<(String, String, String)> = Vec::new();
     for (_index, (name, path)) in projects.iter().enumerate() {
